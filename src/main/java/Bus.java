291652f50a307097481039a1fab6eae2077ffd3a
Siarhei B <bnds1988@gmail.com>
2025-02-11 00:21:18 +0300

    added quicksort algorithm + classes bus, user, student

---
+++ src/main/java/Bus.java
@@ -0,0 +1,81 @@
+import java.util.Objects;
+
+public class Bus implements Comparable<Bus> {
+    private final String number;
+    private final String model;
+    private final int mileage;
+
+    private Bus(BusBuilder builder) {
+        this.number = builder.number;
+        this.model = builder.model;
+        this.mileage = builder.mileage;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getMileage() {
+        return mileage;
+    }
+
+    @Override
+    public int compareTo(Bus other) {
+        int result = this.number.compareTo(other.number);
+        if (result == 0) {
+            result = this.model.compareTo(other.model);
+            if (result == 0) {
+                return Integer.compare(this.mileage, other.mileage);
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Bus bus = (Bus) o;
+        return Objects.equals(number, bus.number) &&
+                Objects.equals(model, bus.model) && mileage == bus.mileage;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number, model, mileage);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Bus [ number: %-10s model: %-10s mileage: %-7d ]", number, model, mileage);
+    }
+
+    public static class BusBuilder {
+        private String number;
+        private String model;
+        private int mileage;
+
+        public BusBuilder setNumber(String number) {
+            this.number = number;
+            return this;
+        }
+
+        public BusBuilder setModel(String model) {
+            this.model = model;
+            return this;
+        }
+
+        public BusBuilder setMileage(int mileage) {
+            this.mileage = mileage;
+            return this;
+        }
+
+        public Bus build() {
+            return new Bus(this);
+        }
+    }
+}
---
+++ src/main/java/User.java
@@ -0,0 +1,82 @@
+import java.util.Objects;
+
+public class User implements Comparable<User> {
+    private final String name;
+    private final String password;
+    private final String email;
+
+    private User(UserBuilder builder) {
+        this.name = builder.name;
+        this.password = builder.password;
+        this.email = builder.email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public int compareTo(User other) {
+        int result = this.name.compareTo(other.name);
+        if (result == 0) {
+            result = this.password.compareTo(other.password);
+            if (result == 0) {
+                return this.email.compareTo(other.email);
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("User [ name: %-10s password: %-10s email: %-10s ]", name, password, email);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(name, user.name) &&
+                Objects.equals(password, user.password) &&
+                Objects.equals(email, user.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, password, email);
+    }
+
+    public static class UserBuilder {
+        private String name;
+        private String password;
+        private String email;
+
+        public UserBuilder setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public UserBuilder setPassword(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public UserBuilder setEmail(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public User build() {
+            return new User(this);
+        }
+    }
+}
