043b79e3f82f20ffed38c3c5279145907dffa405
nastatsan <vnastyap@gmail.com>
2025-02-10 11:36:38 +0300

    Create DataValidation.java

---
+++ src/main/java/DataValidation.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class DataValidation {
+    int dataType;
+
+    public DataValidation(int dataType) {
+        this.dataType = dataType;
+    }
+
+    boolean validation() {
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        return switch (this.dataType) {
+            case 1 -> busValidation(text);
+            case 2 -> userValidation(text);
+            case 3 -> studentValidation(text);
+            default -> false;
+        };
+    }
+
+    boolean busValidation(String text) {
+
+        return false;
+    }
+
+    boolean userValidation(String text) {
+
+        return false;
+    }
+
+    boolean studentValidation(String text) {
+
+        return false;
+    }
+}
