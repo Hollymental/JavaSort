6f98a08deef003fd4df4999355b6fb00494df1a7
Siarhei B <bnds1988@gmail.com>
2025-02-11 14:15:27 +0300

    added strategy

---
+++ src/main/java/QuickSortStrategy.java
@@ -0,0 +1,14 @@
+public class QuickSortStrategy<T extends Comparable<T>> implements SortingStrategy<T> {
+
+    private final QuickSort<T> quickSort;
+
+    public QuickSortStrategy() {
+        this.quickSort = new QuickSort<>();
+    }
+
+    @Override
+    public void sort(T[] array) {
+        quickSort.sort(array);
+    }
+}
+
