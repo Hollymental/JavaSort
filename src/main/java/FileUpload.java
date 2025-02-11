d7c89ebd99363f3e3d242bbc00a4e6e47a806efc
nastatsan <vnastyap@gmail.com>
2025-02-11 11:47:55 +0300

    upload xlsx

--- src/main/java/FileUpload.java
+++ src/main/java/FileUpload.java
@@ -1,9 +1,14 @@
 import java.io.File;
-import java.io.FileReader;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.file.*;
 import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.Iterator;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class FileUpload {
     String fileName;
@@ -15,19 +20,46 @@ public class FileUpload {
     }
 
     void dataUpload() {
-        String file = this.path + this.fileName;
-
+        String filePath = this.path + this.fileName;
         ArrayList<String> list = new ArrayList<>();
         try {
-            Scanner scanner = new Scanner(new FileReader(new File(file)));
-//        scanner.useDelimiter(" ");
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                list.add(line);
+            FileInputStream file = new FileInputStream(new File(filePath));
+            XSSFWorkbook workbook = new XSSFWorkbook(file);
+            XSSFSheet sheet = workbook.getSheetAt(0);
+
+            Iterator<Row> rowIterator = sheet.iterator();
+            while (rowIterator.hasNext()) {
+                Row row = rowIterator.next();
+                Iterator<Cell> cellIterator
+                        = row.cellIterator();
+                while (cellIterator.hasNext()) {
+
+                    Cell cell = cellIterator.next();
+                    switch (cell.getCellType()) {
+
+                        // Case 1
+                        case Cell.CELL_TYPE_NUMERIC:
+                            System.out.print(
+                                    cell.getNumericCellValue()
+                                            + "t");
+                            break;
+
+                        // Case 2
+                        case Cell.CELL_TYPE_STRING:
+                            System.out.print(
+                                    cell.getStringCellValue()
+                                            + "t");
+                            break;
+                    }
+                }
+
+                System.out.println("");
             }
-            System.out.print(list);
+
+            file.close();
         } catch (IOException ex) {
-            System.out.println(ex.getMessage());
+            ex.printStackTrace();
+//            System.out.println(ex.getMessage());
         }
 //        return list.toArray(new String[list.size()]);
     }
