abe0f7ff01b2854a2969f60001a7ad3d24cc7b9a
Siarhei B <bnds1988@gmail.com>
2025-02-12 10:57:00 +0300

    + ForBus + Comparators

---
+++ src/main/java/BusComporators.java
@@ -0,0 +1,22 @@
+public class BusComporators {
+    public static class SortByNumber implements ComparatorStrategy<Bus> {
+        @Override
+        public int compare(Bus b1, Bus b2) {
+            return b1.getNumber().compareTo(b2.getNumber());
+        }
+    }
+
+    public static class SortByModel implements ComparatorStrategy<Bus> {
+        @Override
+        public int compare(Bus b1, Bus b2) {
+            return b1.getModel().compareTo(b2.getModel());
+        }
+    }
+
+    public static class SortByMileage implements ComparatorStrategy<Bus> {
+        @Override
+        public int compare(Bus b1, Bus b2) {
+            return Integer.compare(b1.getMileage(), b2.getMileage());
+        }
+    }
+}
---
+++ src/main/java/StudentComparators.java
@@ -0,0 +1,22 @@
+public class StudentComparators {
+    public static class SortByGroupNumber implements ComparatorStrategy<Student> {
+        @Override
+        public int compare(Student s1, Student s2) {
+            return s1.getGroupNumber().compareTo(s2.getGroupNumber());
+        }
+    }
+
+    public static class SortByAverageScore implements ComparatorStrategy<Student> {
+        @Override
+        public int compare(Student s1, Student s2) {
+            return Double.compare(s1.getAverageScore(),s2.getAverageScore());
+        }
+    }
+
+    public static class SortByRecordBookNumber implements ComparatorStrategy<Student> {
+        @Override
+        public int compare(Student s1, Student s2) {
+            return Integer.compare(s1.getRecordBookNumber(),s2.getRecordBookNumber());
+        }
+    }
+}
